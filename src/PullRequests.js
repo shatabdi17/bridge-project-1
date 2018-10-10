62fbc36ef763914838e67a6ae5e6db251d984164
Shatabdi Bhattacharjee <shatabdi17@gmail.com>
2018-10-10 15:32:21 -0400

    added styles to Pullrequest component

--- src/PullRequests.js
+++ src/PullRequests.js
@@ -2,21 +2,24 @@ import React from "react";
 
 export default ({ events }) => {
 	return (
-		<div className="pull-requests">
+		<div className="pullrequests-container">
 			<h2>Pull Request Events</h2>
+			{events.length > 0 ?
+			<ul className="pullrequests-repolist">
 				{events.map((event, i) => {
 					if (event.type === "PullRequestEvent") {
-						return <div key={`pullRequest-${i}`}>
-									<p>{event.status} PullRequest</p>
-									{event.status === "open" ? <p class="open-pull-request" key={`pullRequestStatus-${i}`}>{event.status}</p> : <p class="closed-pull-request" key={`pullRequestStatus-${i}`}>{event.status}</p>}
-									<p key={`pullRequestEvent-${i}`}>Repo name:{event.repo.name}</p>									
-									<p key={`pullRequestEventURL-${i}`}><a href={event.html_url}>Pull Request URL</a></p>	
+						return <li key={`pullRequest-${i}`}>
+									<p key={`pullRequestEventURL-${i}`}><a className="bold" href={event.html_url}>Repo</a></p>	
+									<p key={`pullRequestEvent-${i}`}>{event.repo.name}</p>									
 									<p key={`pullRequestTitle-${i}`}>{event.title}</p>				
-							</div>
+									{event.status === "open" ? <p class="open-pull-request" key={`pullRequestStatus-${i}`}>{event.status}</p> : <p class="closed-pull-request" key={`pullRequestStatus-${i}`}>{event.status}</p>}
+							</li>
 					} else {
 						return null;
 					}
 				})}
+			</ul>
+			 :<p>No PullRequest events!</p>}
 		</div>
 	)
 }
